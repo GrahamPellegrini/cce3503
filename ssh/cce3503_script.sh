62fb4655cd5412c57ed490074c1bf4ae5e402300
GrahamPellegrini <graham.pellegrini.22@um.edu.mt>
2024-12-12 17:15:57 +0100

    Conclusion left

--- ssh/cce3503_script.sh
+++ ssh/cce3503_script.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-#SBATCH --partition=teaching_gpu
+# ALWAYS specify CPU and RAM resources needed as well as walltime
+##SBATCH --partition=teaching_gpu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-cpu=2G
-#SBATCH --time=0-01:00:00
-#SBATCH --output=/opt/users/gpel0001/cce3503/ssh/out/cce3503_slurm_%A_%a.out
-#SBATCH --error=/opt/users/gpel0001/cce3503/ssh/err/cce3503_slurm_%A_%a.err
-#SBATCH --job-name=cce3503_multi_label_classification
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=200M
+#SBATCH --time=0-10:00:00
+###SBATCH --reservation=cce3015
+
+
+# job parameters
+#SBATCH --output=/opt/users/gpel0001/cce3503/ssh/out/cc3503_slurm_%A_%a.out
+#SBATCH --error=/opt/users/gpel0001/cce3503/ssh/err/cc3503_slurm_%A_%a.err
 #SBATCH --account=undergrad
+
+# email user with progress
 #SBATCH --mail-user=graham.pellegrini.22@um.edu.mt
 #SBATCH --mail-type=all
 
@@ -31,3 +37,6 @@ NOTEBOOK_PATH="/opt/users/gpel0001/cce3503/assignment-2/multi-label-classificati
 jupyter nbconvert --execute --to notebook \
     --allow-errors --inplace \
     "$NOTEBOOK_PATH"
+
+# Indicate that the job has finished
+echo "Job finished successfully"
